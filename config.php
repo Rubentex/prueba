7a0ac635e4b7cee648b0cfc69bdd0c3dc86d2375
Rubentex <77239189+Rubentex@users.noreply.github.com>
2021-08-05 19:32:56 -0400

    s

--- config.php
+++ config.php
@@ -1,31 +1,26 @@
 <?php
 // Array of the files with an unique ID
 $files = array(
-    'UID12345' => array(
+    'FID12345' => array(
         'content_type' => 'application/zip', 
-        'suggested_name' => 'codex.zip', 
+        'suggested_name' => 'tutorials-file.zip', 
         'file_path' => 'tempfile.zip',
         'type' => 'local_file'
-    ),
-    'UID67890' => array(
-        'content_type' => 'audio/mpeg', 
-        'suggested_name' => 'codex2.zip', 
-        'file_path' => 'tempfile2.zip',
-        'type' => 'remote_file'
-    ),
+    )
+    
 );
 
 // Base URL of the application
-define('BASE_URL','http://'. $_SERVER['HTTP_HOST'].'/generate_temporary_download_link_php/');
+define('BASE_URL','http://'. $_SERVER['HTTP_HOST'].'/');
 
-// Path of the download.php file
-define('DOWNLOAD_PATH', BASE_URL.'download.php');
+// Path of the download-link.php file
+define('DOWNLOAD_PATH', BASE_URL.'download-link.php');
 
 // Path of the token directory to store keys
 define('TOKEN_DIR', 'tokens');
 
 // Authentication password to generate download links
-define('OAUTH_PASSWORD','CODEXWORLD');
+define('OAUTH_PASSWORD','Tutorialswebsite');
 
 // Expiration time of the link (examples: +1 year, +1 month, +5 days, +10 hours)
 define('EXPIRATION_TIME', '+5 minutes');
--- index.php
+++ index.php
@@ -1,17 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Document</title>
-		<style type="text/css">
-		.container{padding: 20px;}
-		</style>
-	</head>
-	<body>
-		<div class="container">
-			<h1>va expirar</h1>
-			<p><a href="generate.php?CODEXWORLD">Generate download link</a></p>
-		</div>
-	</body>
-</html>
+<a href="generate-link.php?Tutorialswebsite">Generate download link</a>
