05260e670ef168029ca5b7d2e76f33cd1e3a8cc4
Rubentex <77239189+Rubentex@users.noreply.github.com>
2021-08-06 15:29:10 -0400

    d

--- config.php
+++ config.php
@@ -1,18 +1,18 @@
 <?php
 // Array of the files with an unique ID
 $files = array(
-    'UID12345' => array(
-        'content_type' => 'application/zip', 
-        'suggested_name' => 'codex.zip', 
-        'file_path' => 'files/tempfile.zip',
-        'type' => 'local_file'
-    ),
-    'UID67890' => array(
-        'content_type' => 'application/zip', 
-        'suggested_name' => 'codex2.zip', 
-        'file_path' => 'files/tempfile.zip',
-        'type' => 'local_file'
-    ),
+'UID12345' => array(
+'content_type' => 'application/zip',
+'suggested_name' => 'codex.zip',
+'file_path' => 'files/tempfile.zip',
+'type' => 'local_file'
+),
+'UID67890' => array(
+'content_type' => 'application/zip',
+'suggested_name' => 'codex2.zip',
+'file_path' => 'files/tempfile.zip',
+'type' => 'local_file'
+),
 );
 // Base URL of the application
 define('BASE_URL','http://'. $_SERVER['HTTP_HOST'].'/');
@@ -23,6 +23,8 @@ define('TOKEN_DIR', 'tokens');
 // Authentication password to generate download links
 define('OAUTH_PASSWORD','CODEXWORLD');
 // Expiration time of the link (examples: +1 year, +1 month, +5 days, +10 hours)
-define('EXPIRATION_TIME', '+5 minutes');
-// Expiration time of the link (examples: +5)
-define('CLICK_NUMBER_EXPIRATION','+5');
+define('EXPIRATION_TIME', '+5 minutes,+5 click');
+// Don't worry about this
+header("Cache-Control: no-cache, must-revalidate");
+header("Expires: ".date('U', strtotime(EXPIRATION_TIME)));
+?>
