3769ce62b7d5f3c62248dfac23987d912ddc9900
Rubentex <77239189+Rubentex@users.noreply.github.com>
2021-08-06 18:28:04 -0400

    s

--- config.php
+++ config.php
@@ -15,7 +15,7 @@ $files = array(
 ),
 );
 // Base URL of the application
-define('BASE_URL','http://'. $_SERVER['HTTP_HOST'].'/Temporary_Download_link/');
+define('BASE_URL','http://'. $_SERVER['HTTP_HOST'].'/');
 // Path of the download-link.php file
 define('DOWNLOAD_PATH', BASE_URL.'download.php');
 // Path of the token directory to store keys
@@ -23,7 +23,7 @@ define('TOKEN_DIR', 'tokens');
 // Authentication password to generate download links
 define('OAUTH_PASSWORD','CODEXWORLD');
 // Expiration time of the link (examples: +1 year, +1 month, +5 days, +10 hours)
-define('EXPIRATION_TIME', '+1 month,+5 click');
+define('EXPIRATION_TIME', '+1 month,');
 // Don't worry about this
 header("Cache-Control: no-cache, must-revalidate");
 header("Expires: ".date('U', strtotime(EXPIRATION_TIME)));
