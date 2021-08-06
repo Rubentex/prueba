a3919e55e40418eabe4468f7ec9f1b89be3395a0
Rubentex <77239189+Rubentex@users.noreply.github.com>
2021-08-05 20:16:28 -0400

    s

--- config.php
+++ config.php
@@ -10,7 +10,7 @@ $files = array(
 
 );
 // Base URL of the application
-define('BASE_URL','http://'. $_SERVER['HTTP_HOST'].'/');
+define('BASE_URL','http://'. $_SERVER['HTTP_HOST'].'/generate_temporary_download_link_php/');
 // Path of the download-link.php file
 define('DOWNLOAD_PATH', BASE_URL.'download-link.php');
 // Path of the token directory to store keys
